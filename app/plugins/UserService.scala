a374eeb4f8ce6073e74c93b9dc7904211c6df68f
Christian Papauschek <christian@papauschek.at>
2013-12-27 15:07:45 +0100

    basic securesocial login

---
+++ app/plugins/UserService.scala
@@ -0,0 +1,92 @@
+package plugins
+
+import play.api._
+import securesocial.core.{Identity, IdentityId, UserServicePlugin}
+import securesocial.core.providers.Token
+
+class UserService(application: Application) extends UserServicePlugin(application) {
+  /**
+   * Finds a user that maches the specified id
+   *
+   * @param id the user id
+   * @return an optional user
+   */
+  def find(id: IdentityId):Option[Identity] = {
+    ???
+  }
+
+  /**
+   * Finds a user by email and provider id.
+   *
+   * Note: If you do not plan to use the UsernamePassword provider just provide en empty
+   * implementation.
+   *
+   * @param email - the user email
+   * @param providerId - the provider id
+   * @return
+   */
+  def findByEmailAndProvider(email: String, providerId: String):Option[Identity] =
+  {
+    ???
+  }
+
+  /**
+   * Saves the user.  This method gets called when a user logs in.
+   * This is your chance to save the user information in your backing store.
+   * @param user
+   */
+  def save(user: Identity) : Identity = {
+    ???
+  }
+
+  /**
+   * Saves a token.  This is needed for users that
+   * are creating an account in the system instead of using one in a 3rd party system.
+   *
+   * Note: If you do not plan to use the UsernamePassword provider just provide en empty
+   * implementation
+   *
+   * @param token The token to save
+   * @return A string with a uuid that will be embedded in the welcome email.
+   */
+  def save(token: Token) = {
+    ???
+  }
+
+
+  /**
+   * Finds a token
+   *
+   * Note: If you do not plan to use the UsernamePassword provider just provide en empty
+   * implementation
+   *
+   * @param token the token id
+   * @return
+   */
+  def findToken(token: String): Option[Token] = {
+    ???
+  }
+
+  /**
+   * Deletes a token
+   *
+   * Note: If you do not plan to use the UsernamePassword provider just provide en empty
+   * implementation
+   *
+   * @param uuid the token id
+   */
+  def deleteToken(uuid: String) {
+    ???
+  }
+
+  /**
+   * Deletes all expired tokens
+   *
+   * Note: If you do not plan to use the UsernamePassword provider just provide en empty
+   * implementation
+   *
+   */
+  def deleteExpiredTokens() {
+    ???
+  }
+}
--- project/Build.scala
+++ project/Build.scala
@@ -11,10 +11,13 @@ object ApplicationBuild extends Build {
     jdbc,
     cache, // play cache external module
     "com.typesafe.slick" % "slick_2.10" % "1.0.1", // database mapper
-    "mysql" % "mysql-connector-java" % "5.1.22"
+    "mysql" % "mysql-connector-java" % "5.1.22",
+    "securesocial" %% "securesocial" % "2.1.2"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
+
+    resolvers += Resolver.url("sbt-plugin-releases", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns),
 
     // disable less compilation
     lessEntryPoints <<= (sourceDirectory in Compile)(base => (
